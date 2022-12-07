78448bdc8ae14d4f97c0211416ad6428bbb88f16
Lasse Larsen <iamlasse@gmail.com>
2022-12-07 12:40:48 +0100

    Changed text

--- src/components/tasks/01/task-01.tsx
+++ src/components/tasks/01/task-01.tsx
@@ -8,7 +8,7 @@ export const Task01: React.FC = () => {
             <div className={styles.progress}>
                 <b>01</b> / 10
             </div>
-            <div className={styles.title}>Let's Start!</div>
+            <div className={styles.title}>Let&apos;s GO!</div>
             <ol start={0} className={styles.desc}>
                 <li>
                     In this tutorial, you'll learn how you can view and visually
